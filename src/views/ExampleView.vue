974b40eabb7a900f70577872464d060cb228c452
Greg Tambornino <greg.tambornino@concordusa.com>
2023-09-08 17:31:16 -0500

    random styling

--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -16,32 +16,32 @@ function addTodo() {
   newTodo.value = ''
 }
 
-function removeTodo(todo: { id: number, text: string, done: boolean }) {
+function removeTodo(todo: { id: number; text: string; done: boolean }) {
   todos.value = todos.value.filter((t) => t !== todo)
 }
-  
-  const filteredTodos = computed(() => {
-    return hideCompleted.value 
-      ? todos.value.filter(todo => !todo.done)
-    	: todos.value
-  })
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((todo) => !todo.done) : todos.value
+})
 </script>
 
-<template class="example">
-  <form @submit.prevent="addTodo" class="green">
-    <input v-model="newTodo">
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+<template>
+  <div class="example">
+    <form @submit.prevent="addTodo" class="todo-form">
+      <input v-model="newTodo" />
+      <button class="vue-btn">Add Todo</button>
+    </form>
+    <ul>
+      <li v-for="todo in filteredTodos" :key="todo.id">
+        <input type="checkbox" v-model="todo.done" />
+        <span :class="{ done: todo.done }">{{ todo.text }}</span>
+        <button @click="removeTodo(todo)" class="delete-btn">X</button>
+      </li>
+    </ul>
+    <button @click="hideCompleted = !hideCompleted" class="bottom-btn vue-btn">
+      {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+    </button>
+  </div>
 </template>
 
 <style>
@@ -49,11 +49,49 @@ function removeTodo(todo: { id: number, text: string, done: boolean }) {
   text-decoration: line-through;
 }
 
+form {
+    display: flex;
+    justify-content: space-between;
+    padding: 1vw;
+}
+
+input {
+    margin-right: .5vw;
+    min-height: 3vw;
+    border-radius: 10px;
+    border-color: inherit;
+    background-color: inherit;
+}
+
+ul {
+    margin: 1vw;
+}
+.delete-btn {
+    background-color: greenyellow;
+    border: none;
+    border-radius: .1rem;
+    margin-left: 1vw;
+}
+
+.bottom-btn { 
+    margin: 1vw;
+}
+
+.vue-btn {
+    padding: .6vw;
+    background-color: green;
+    color: aliceblue;
+    border: 0px;
+    border-radius: .7rem;
+}
+
 @media (min-width: 1024px) {
-    .example {
-        min-height: 100vh;
-        /* display: grid;
-        align-items: center; */
-    }
+  .example {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
