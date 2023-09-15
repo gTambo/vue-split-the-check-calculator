929d49719da4b424f4f264fd576ce074aeb8c008
Greg Tambornino <greg.tambornino@concordusa.com>
2023-09-15 15:08:40 -0500

    feature for individual items add

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,104 @@
 <script setup lang="ts">
-  import { ref, nextTick } from 'vue';
-  const msg = ref('enter your percentages')
-  const payers = ref(0)
-  const total = ref(0)
-  const id = ref(0)
-  const payerName = ref('')
-  // function incrementId() {
-  //   id.value++
-  // }
+import { ref } from 'vue';
 
-  const percentageList = ref(new Array<{id: String, name: String, percent: number, payment: number}>)
-  
-  function clearList(list: Array<any>) {
-    if(list.length) {
-      let length = list.length
-      for(let i = 0; i < length; i++) {
-        list.pop();
-      }
+const total = ref(0)
+const payerId = ref(0)
+const payerName = ref('')
+const itemNo = ref(1)
+const itemName = ref('')
+const itemCost = ref(0.00)
+const payersList = ref(new Array<{ payerId: String, name: String, percent: number, payment: number }>)
+const itemsList = ref(new Array<{ itemNo: String, itemName: String, cost: number } >)
+
+// function increment(x: number) {
+//   return x++
+// }
+
+function clearList(list: Array<any>) {
+  if (list.length) {
+    let length = list.length
+    for (let i = 0; i < length; i++) {
+      list.pop();
     }
   }
-  async function createInputs() {
+}
+async function addPayer() {
 
-    percentageList.value.push({ id: id.value.toString(), name: payerName.value, percent: 0, payment: 0})
-    id.value++
-    payerName.value = ''
-    // if(percentageList.value.length != payers.value) {
-    //   clearList(percentageList.value)
-    //   for(let i = 0; i < payers.value; i++) {
-    //     // create percentage ref and add to percentage list
-    //     const id = (i + 1).toString()
-    //     let newName = 'percentage' + id
-    //     let payer = { id: id, name: newName, percent: 0, payment: 0 }
+  payersList.value.push({ payerId: payerId.value.toString(), name: payerName.value, percent: 0, payment: 0 })
+  payerId.value++
+  payerName.value = ''
+}
 
-    //     percentageList.value.push(payer)
-        
-    //   }
-    // }
-    // payers.value = 0
-    // await nextTick()
-    
+function updateTotal(newItem: number) {
+  total.value = total.value + newItem
+}
+
+function addItem() {
+  itemsList.value.push({ itemNo: itemNo.value.toString(), itemName: itemName.value, cost: itemCost.value})
+  updateTotal(itemCost.value)
+  itemCost.value = 0
+  itemName.value = ''
+  itemNo.value++
+}
+
+function verifyPercentages(percentages: Array<number>): boolean {
+  if (percentages.reduce((x, y) => x + y, 0) == 100) return true
+  return false
+}
+
+function calculate() {
+  if (verifyPercentages(payersList.value.map(payer => payer.percent))) {
+    payersList.value.map(payer => (payer.payment = total.value * payer.percent / 100))
   }
+  else { alert("percentages must total to 100") }
 
-  function verifyPercentages(percentages: Array<number>): boolean {
-    if(percentages.reduce((x, y) => x + y, 0) == 100) return true
-    return false
-  }
-
-  function calculate() {
-    if(verifyPercentages(percentageList.value.map(payer => payer.percent))){
-      percentageList.value.map(payer => (payer.payment = total.value * payer.percent / 100))
-    }
-    else {alert("percentages must total to 100")}
-
-  }
+}
 </script>
 
 <template>
   <div>
     <h1>Payment Calculator</h1>
 
-    <form action="submit" @submit.prevent="createInputs">
+    <form action="submit" @submit.prevent="addPayer">
       <label for="payers">Name: </label>
-      <input v-model="payerName" id="payers" type="text" placeholder="payer" >
+      <input v-model="payerName" id="payers" type="text" placeholder="payer">
       <button type="submit">Set as Payer</button>
     </form>
-    
+    <form action="submit" @submit.prevent="addItem">
+      <label for="item-name">Enter an item: </label>
+      <input v-model="itemName" id="item-name" type="text" >
+      <label for="item-cost">Item cost: </label>
+      $<input v-model="itemCost" id="item-cost" type="number" min=".01" step=".01" max="1000000">
+      <button type="submit">Add Item</button>
+    </form>
+    <div v-if="itemsList.length">
+      <div v-for="item in itemsList" :key="'item' + item.itemNo">
+        <p>{{ (item.itemNo).toString() }}. {{ item.itemName }}, ${{ item.cost }}</p>
+      </div>
+    </div>
+    <p class="text" >Items total: ${{ total }}</p>
     <form action="submit" @submit.prevent="calculate()">
-      <label for="total-amount" class="text">Enter the total: </label>
-      $<input v-model="total" id="total-amount" type="number" min=".01" step=".01" max="1000000">
-      <p class="text">{{ msg }}</p>
-      
-      <div v-if="percentageList.length">
-        <div v-for="payer in percentageList" :key="'payer' + payer.id">
-        <label :for="'percent' + payer.id">{{ payer.name }} $</label>
-        <input v-model="payer.percent" :id="'percent' + payer.id" type="number" min="0.01" max="100" step=".01"/>
+      <p class="text">Enter your percentages</p>
+      <div v-if="payersList.length">
+        <div v-for="payer in payersList" :key="'payer' + payer.payerId">
+          <label :for="'percent' + payer.payerId">{{ payer.name }} $</label>
+          <input v-model="payer.percent" :id="'percent' + payer.payerId" type="number" min="0.01" max="100" step=".01" />
         </div>
       </div>
       <button type="submit">Calculate totals</button>
-      <button @click.prevent="clearList(percentageList)">Clear list of payers</button>
+      <button @click.prevent="clearList(payersList)">Clear list of payers</button>
     </form>
-    
-    
-    <p class="text" v-for="payer in percentageList" :key="'total' + payer.id">1: ${{ payer.payment.toFixed(2) }}, <br/></p>
+
+
+    <p class="text" v-for="payer in payersList" :key="'total' + payer.payerId">1: ${{ payer.payment.toFixed(2) }}, <br /></p>
   </div>
 </template>
 
 <style scoped>
-  .text {
-    font-family: Georgia, 'Times New Roman', Times, serif;
-    font-weight: 900;
-    font-size: 35px;
-    font-style: oblique;
-  }
+.text {
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-weight: 900;
+  font-size: 35px;
+  font-style: oblique;
+}
 </style>
